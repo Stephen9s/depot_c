554d6df449c45621705eab1caea9a77cb4a6d385
Stephen Neyens <neyens.s@gmail.com>
2012-10-01 21:31:01 -0400

    Work

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,10 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 class Product < ActiveRecord::Base
+  has_many :line_items
+  
+  before_destroy :ensure_not_referenced_by_any_line_item
+    
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -17,7 +21,13 @@ class Product < ActiveRecord::Base
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
   
-  def self.find_products_for_sale
-    find(:all, :order => "title")
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
   end
 end
